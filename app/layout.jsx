538168935eafa8f27c2d6263b2b426fb6d161719
Prateek Sahay <ptkinvent@gmail.com>
2026-02-16 18:34:37 -0500

    Improved dark mode

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,30 +1,21 @@
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 export default function MainLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta charSet="UTF-8" />
         <title>Scaffold</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
         <meta name="description" content="Scaffold." />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                (function() {
-                  try {
-                    var darkMode = localStorage.getItem('darkMode') === 'true';
-                    if (darkMode) {
-                      document.documentElement.classList.add('dark');
-                    }
-                  } catch (e) {}
-                })();
-              `,
-          }}
-        />
       </head>
-      <body className="h-full bg-white dark:bg-secondary-800">{children}</body>
+      <body className="h-full bg-white dark:bg-secondary-800">
+        <ThemeProvider attribute="class" enableSystem={false}>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
